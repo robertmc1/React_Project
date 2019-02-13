09b4fe738a1f4f56daba6529bda7592ebb76afd6
Robert Machi <robert.mc@hotmail.es>
2019-02-13 13:23:51 +0100

    role model and router created

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = +process.argv.slice(2)[0] || process.env.PORT || 3001;
+require('./config/mongoose');
 
 app.use(bodyParser.json());
 
