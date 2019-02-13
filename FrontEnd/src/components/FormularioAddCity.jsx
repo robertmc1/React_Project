d25784ae3b3060ef97d79bdd811f0228ec40bd1a
Robert Machi <robert.mc@hotmail.es>
2019-02-13 21:46:22 +0100

    Error ortografico (Headquarter)

--- FrontEnd/src/components/FormularioAddCity.jsx
+++ FrontEnd/src/components/FormularioAddCity.jsx
@@ -97,7 +97,7 @@ class FormularioAddCity extends Component {
                         <div id="divFormRight">
                             <div id="divForm" >
                                 <p id="textoGrande"><b> Demium team</b></p>
-                                <label htmlFor="Headquorter">Team:</label>
+                                <label htmlFor="Headquarter">Team:</label>
                                 <select
                                     name="team"
                                     onChange={this.handleInput}>
